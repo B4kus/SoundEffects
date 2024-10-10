125a7863d166336b0c43ee186c2eb6ad00e3bc75
Szymon Szysz <szysz.szymon@gmail.com>
2024-10-10 19:38:47 +0200

    Add documentation and update tests

--- Sources/Manager/SoundEffectManager.swift
+++ Sources/Manager/SoundEffectManager.swift
@@ -9,43 +9,73 @@ import Foundation
 import AudioToolbox
 
 protocol SoundEffectMangerProtocol {
-    func perLoadSoundEffects(soundEffects: [SoundEffect], inbundle: Bundle)
+    func perLoadSoundEffects(soundEffects: [SoundEffect],in bundle: Bundle)
     func play(soundEffect: SoundEffect)
     func unload(soundEffect: SoundEffect)
     var onSoundEffectManagerError: ((SoundEffectMangerError) -> Void)? { get }
 }
 
-
+/// `SoundEffectManager` is a singleton class that manages the registration, playback,
+/// and unloading of sound effects using the `AudioServices` system sound API.
+/// It supports debugging with logging functionality and error handling.
+///
+/// - Note: The sound effects are cached using a dictionary to store `SystemSoundID`
+///   for quick access.
 final class SoundEffectManager: SoundEffectMangerProtocol {
     
-    // MARK: - Shared
+    // MARK: - Shared Instance
     
+    /// The shared instance of `SoundEffectManager` to be used globally.
+    ///
+    /// - Important: Access this instance on the main thread, since the `@MainActor`
+    ///   annotation ensures that operations are performed safely on the main queue.
     @MainActor static let shared = SoundEffectManager()
+
+    // MARK: - Properties
     
-    // MARK: - Private Properties
-    
+    /// A dictionary that maps sound file names to their corresponding `SystemSoundID`.
+    /// This serves as the cache for the sound effects that have been registered.
+    ///
+    /// - Note: The key is the full filename (`name.extension`) of the sound file.
     private(set) var soundIDs: [String: SystemSoundID] = [:]
     
-    // MARK: - Debug Properties
-    
+    /// Enables or disables debug logging for the `SoundEffectManager`. When `true`,
+    /// debug logs will be printed or sent to a custom `debugLogger` if provided.
+    ///
+    /// - Default: `false`
     var isDebugMode: Bool = false
+    
+    /// A custom logging function to be used for debug messages. If no logger is provided,
+    /// the debug messages will be printed to the console.
     var debugLogger: ((String) -> Void)?
     
-    // MARK: - Error Callback
-    
+    /// A closure that is triggered when an error occurs in the manager. The closure
+    /// provides a `SoundEffectMangerError` object indicating the type of error encountered.
+    ///
+    /// - SeeAlso: `SoundEffectMangerError`
     var onSoundEffectManagerError: ((SoundEffectMangerError) -> Void)?
     
     // MARK: - Initialization
     
+    /// Private initializer to ensure the singleton pattern.
     private init() { }
+
+    // MARK: - Public Methods
     
-    // MARK: - Register Sound Effects
-    
-    func perLoadSoundEffects(soundEffects: [SoundEffect], inbundle: Bundle = .main) {
+    /// Preloads an array of sound effects, loading them from the specified bundle into memory.
+    /// If the sound effect is already registered, it will be skipped.
+    ///
+    /// - Parameters:
+    ///   - soundEffects: An array of `SoundEffect` objects to be preloaded.
+    ///   - bundle: The bundle from which to load the sound files. Defaults to the main bundle.
+    ///
+    /// - Important: Ensure that the sound files are correctly named and available
+    ///   in the bundle, otherwise an error will be thrown.
+    func perLoadSoundEffects(soundEffects: [SoundEffect], in bundle: Bundle = .main) {
         for soundEffect in soundEffects {
             logDebug("Registering sound effect: \(soundEffect.fullFileName)")
             if soundIDs[soundEffect.fullFileName] == nil {
-                if let soundID = loadSound(named: soundEffect.name, fileExtension: soundEffect.fileExtension) {
+                if let soundID = loadSound(named: soundEffect.name, fileExtension: soundEffect.fileExtension, bundle: bundle) {
                     soundIDs[soundEffect.fullFileName] = soundID
                     logDebug("Successfully loaded sound effect \(soundEffect.fullFileName) with ID \(soundID)")
                 } else {
@@ -57,8 +87,12 @@ final class SoundEffectManager: SoundEffectMangerProtocol {
         }
     }
     
-    // MARK: - Play Sound Effects
-    
+    /// Plays the specified sound effect. The sound effect must be registered (preloaded) first.
+    ///
+    /// - Parameter soundEffect: The `SoundEffect` object representing the sound to be played.
+    ///
+    /// - Note: If the sound effect is not registered, an error is triggered, and the
+    ///   `onSoundEffectManagerError` closure is called with `.fileNotFound`.
     func play(soundEffect: SoundEffect) {
         logDebug("Attempting to play sound effect: \(soundEffect.fullFileName)")
         if let soundID = soundIDs[soundEffect.fullFileName] {
@@ -70,8 +104,12 @@ final class SoundEffectManager: SoundEffectMangerProtocol {
         }
     }
     
-    // MARK: - Unload Sound Effects
-    
+    /// Unloads the specified sound effect from memory, freeing up system resources.
+    ///
+    /// - Parameter soundEffect: The `SoundEffect` object representing the sound to be unloaded.
+    ///
+    /// - Note: If the sound effect is not found in the cache, an error is logged but no
+    ///   exception is thrown.
     func unload(soundEffect: SoundEffect) {
         logDebug("Unloading sound effect: \(soundEffect.fullFileName)")
         if let soundID = soundIDs[soundEffect.fullFileName] {
@@ -82,12 +120,20 @@ final class SoundEffectManager: SoundEffectMangerProtocol {
             logDebug("Sound effect \(soundEffect.fullFileName) not found in cache.")
         }
     }
+
+    // MARK: - Private Methods
     
-    // MARK: - Load Sound Effects
-    
-    private func loadSound(named: String, fileExtension: String) -> SystemSoundID? {
+    /// Loads a sound effect from the specified bundle and returns the corresponding `SystemSoundID`.
+    ///
+    /// - Parameters:
+    ///   - named: The name of the sound file without the file extension.
+    ///   - fileExtension: The file extension (e.g., "wav" or "mp3").
+    ///   - bundle: The bundle from which to load the sound file.
+    ///
+    /// - Returns: A `SystemSoundID` if the sound was loaded successfully, otherwise `nil`.
+    private func loadSound(named: String, fileExtension: String, bundle: Bundle) -> SystemSoundID? {
         logDebug("Loading sound: \(named).\(fileExtension)")
-        guard let soundURL = Bundle.main.url(forResource: named, withExtension: fileExtension) else {
+        guard let soundURL = bundle.url(forResource: named, withExtension: fileExtension) else {
             logDebug("Error: Sound file \(named).\(fileExtension) not found.")
             onSoundEffectManagerError?(.fileNotFound)
             return nil
@@ -106,8 +152,10 @@ final class SoundEffectManager: SoundEffectMangerProtocol {
         }
     }
     
-    // MARK: - Debug Log Function
-    
+    /// Logs debug messages if `isDebugMode` is enabled. If a custom logger is provided,
+    /// it will be used to log messages, otherwise, messages will be printed to the console.
+    ///
+    /// - Parameter message: The message to be logged.
     private func logDebug(_ message: String) {
         if isDebugMode {
             debugLogger?(message) ?? print("Debug: \(message)")
--- Tests/SoundEffectsTests/SoundEffectsTests.swift
+++ Tests/SoundEffectsTests/SoundEffectsTests.swift
@@ -8,32 +8,16 @@ struct SoundEffectManagerTests {
     @MainActor @Test("Register a sound effect")
     func testRegisterSoundEffect() {
         let soundEffect = SoundEffect(name: "testSound", fileExtension: "wav")
-        SoundEffectManager.shared.perLoadSoundEffects(soundEffects: [soundEffect], inbundle: Bundle.module)
+        SoundEffectManager.shared.perLoadSoundEffects(soundEffects: [soundEffect], in: .module)
         
         #expect(SoundEffectManager.shared.soundIDs[soundEffect.fullFileName] != nil)
     }
-
-    // Test playing a registered sound effect
-//    @MainActor @Test("Play registered sound effect")
-//    func testPlayRegisteredSoundEffect() {
-//        let soundEffect = SoundEffect(name: "testSound", fileExtension: "wav")
-//        SoundEffectManager.shared.register(soundEffects: [soundEffect], inbundle: Bundle.module)
-//        
-//        // Mock a function to capture AudioServicesPlaySystemSound calls
-//        var didPlaySound = false
-//    
-//        AudioServicesPlaySystemSoundWithCompletion(SoundEffectManager.shared.soundIDs[soundEffect.fullFileName] ?? SystemSoundID(1016)) {
-//            print("TEst")
-//            didPlaySound = true
-//        }
-//        #expect(didPlaySound)
-//    }
     
     // Test unloading a sound effect
     @MainActor @Test("Unload a sound effect")
     func testUnloadSoundEffect() {
         let soundEffect = SoundEffect(name: "testSound", fileExtension: "wav")
-        SoundEffectManager.shared.perLoadSoundEffects(soundEffects: [soundEffect], inbundle: Bundle.module)
+        SoundEffectManager.shared.perLoadSoundEffects(soundEffects: [soundEffect], in: .module)
         SoundEffectManager.shared.unload(soundEffect: soundEffect)
         
         #expect(SoundEffectManager.shared.soundIDs[soundEffect.fullFileName] == nil)
